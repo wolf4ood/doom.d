b199c8c0985b62c672dfe353451e79a9b260f886
Enrico Risa <enrico.risa@prima.it>
2020-10-15 19:37:37 +0200

    added hook on save for formatting in elixir mode

--- config.el
+++ config.el
@@ -58,7 +58,13 @@
 (blink-cursor-mode 10)
 
 (add-hook 'before-save-hook (lambda () (when (eq 'rustic-mode major-mode)
-                                           (lsp-format-buffer))))
+                                    (lsp-format-buffer))))
+
+(add-hook 'before-save-hook (lambda () (when (eq 'elixir-mode major-mode)
+                                    (lsp-format-buffer))))
+
+
+
 (use-package lsp-mode
     :commands lsp
     :ensure t

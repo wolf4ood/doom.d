de9a0f6bb05bd0dea975a3db072dda53af3817d4
wolf4ood <enrico.risa@gmail.com>
2021-04-20 22:01:26 +0200

    re-enabled evil

--- init.el
+++ init.el
@@ -51,14 +51,14 @@
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
-       ;:editor
-       ;(evil +everywhere); come to the dark side, we have cookies
-       ;file-templates    ; auto-snippets for empty files
-       ;fold              ; (nigh) universal code folding
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates

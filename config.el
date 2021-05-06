66468eeedc26e9e6111c938ee1e738b09a85fcc1
wolf4ood <enrico.risa@gmail.com>
2021-05-06 22:26:30 +0200

    added word as symbol in movement

--- config.el
+++ config.el
@@ -74,3 +74,9 @@
 (setq company-dabbrev-downcase 0)
 (setq company-idle-delay 0)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
+(with-eval-after-load 'evil
+    (defalias #'forward-evil-word #'forward-evil-symbol)
+    ;; make evil-search-word look for symbol rather than word boundaries
+    (setq-default evil-symbol-word-search t))

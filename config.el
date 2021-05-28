66ba96786664b247d5ae4c212370f6fb9cf79132
Enrico Risa <enrico.risa@prima.it>
2021-05-28 14:43:43 +0200

    added hook on save for elixir-mode

--- config.el
+++ config.el
@@ -59,6 +59,10 @@
 
 (add-hook 'before-save-hook (lambda () (when (eq 'rustic-mode major-mode)
                                     (lsp-format-buffer))))
+
+(add-hook 'before-save-hook (lambda () (when (eq 'elixir-mode major-mode)
+                                    (lsp-format-buffer))))
+
 (add-hook 'after-save-hook (lambda () (when (eq 'org-mode major-mode)
                                         (org-re-reveal-export-to-html))))
 
